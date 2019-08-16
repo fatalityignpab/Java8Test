62a0af950cae0d163cdf388d3f5fa86379cedb48
Ignacio Pablo <ignaciopabloro2@gmail.com>
2019-08-16 01:59:44 -0500

    6 Interfaces Funcionales

--- src/main/java/com/nachojava/Operacion.java
+++ src/main/java/com/nachojava/Operacion.java
@@ -1,6 +1,18 @@
 package com.nachojava;
 
+/* + interfaz funcional es aquella que solamente define una única operación un único método
+
++ no es una interfaz funcional porque por obvias razones tiene más de un método definido*/
+@FunctionalInterface
 public interface Operacion {
+    /*
+     * interfaz funcional es aquella que solamente define una única operación un
+     * único método
+     */
     double calcularPromedio(double n1, double n2);
+
+    //int calcular2();
     // double calcularPromedio();
+
+    /* <String> Genérico - Java 7 */
 }
